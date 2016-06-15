70b8309790c7c6b6d0a2cce3d12e4094471c113f
Yasmín <Yasmn@Yasmn-PC>
2016-06-15 13:51:16 +0200

    Creación de rama grafica e inserción de elementos y dígito a averiguar por ventanas

--- src/busqueda/Busqueda.java
+++ src/busqueda/Busqueda.java
@@ -1,15 +1,25 @@
 package busqueda;
 
+import javax.swing.JOptionPane;
+
 public class Busqueda {
 
     public static void main(String[] args) {
-        int[] numeros = new int[100];
+        int valorIndice;
+        String pedirIndice = JOptionPane.showInputDialog(null, "Introduce el valor del que deseas averiguar el indice: ");  
+        valorIndice = Integer.parseInt(pedirIndice);
+        
+        int[] numeros = new int[valorIndice];
         for (int i = 0; i < numeros.length; i++) {
             numeros[i] = i * 5;
         }
+        int valor;
+        String pedirValor = JOptionPane.showInputDialog(null, "Introduce el valor del que deseas averiguar el indice: ");     
+        valor = Integer.parseInt(pedirValor);   
+        
         Busqueda ejemplo = new Busqueda();
-        int indice = ejemplo.busquedaBinaria(numeros, 450, 0, numeros.length - 1);
-        System.out.println("El indice del valor '450' es: " + indice);
+        int indice = ejemplo.busquedaBinaria(numeros, valor, 0, numeros.length - 1);
+        JOptionPane.showMessageDialog(null,"El indice del valor '"+valor+"' es: " + indice);
     }
 
     public int busquedaBinaria(int[] listado, int clave, int posInicial, int posFinal) {
